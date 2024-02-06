ff1b3047c092ebddfc297d6979cedf5c9cf0bec8
s-h-u-n-u-s-u-k-e-s-a-t-o-u <159138891+s-h-u-n-u-s-u-k-e-s-a-t-o-u@users.noreply.github.com>
2024-02-06 20:59:37 +0900

    Create TwiImg.js
    First file

---
+++ TwiImg.js
@@ -0,0 +1,1 @@
+javascript:function z(){for(var b=document.getElementsByTagName("article")[0].getElementsByClassName("css-9pa8cd"),a=1;a<b.length;a++){var c=b.item(a).getAttribute("src").split("&")[0].replace(/\?.*=/i,%22.%22).replace(/\.webp$/i,%22.jpg%22);window.open(c)}}z();
